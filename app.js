9761f6403801786995e8399e0e198ed44e942662
s4ngs00 <songgurark@gmail.com>
2023-06-23 21:58:36 +0900

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 var server = http.createServer(function (req, res){
 	res.writeHead(200, {'Content-Type' : 'text/plain'});
-	res.end('Hello World sfasdfasdf/home');
+	res.end('Hello World isadfsadfsadf s');
 });
 
 server.listen(8000);
