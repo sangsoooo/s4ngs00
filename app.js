dc56cdcbc173488987ccab479aeb6a4518a9a5ae
s4ngs00 <songgurark@gmail.com>
2023-06-23 21:13:19 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+var server = http.createServer(function (req, res){
+	res.writeHead(200, {'Content-Type' : 'text/plain'});
+	res.end('Hello World sfasdfasdf/home');
+});
+
+server.listen(8000);
